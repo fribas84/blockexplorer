edbffba6222ed7eba4c55ddd0c843361b8dd6a59
Fernando Ribas <fribas84@gmail.com>
2023-06-08 17:19:42 -0300

    refactored block table to a tw styled component

--- src/components/Block.jsx
+++ src/components/Block.jsx
@@ -1,8 +1,37 @@
 import React, { useEffect, useState } from 'react';
 import { Utils } from 'alchemy-sdk';
+import tw from "tailwind-styled-components";
 
+const Div = tw.div`
+relative
+overflow-x-auto
+shadow-md
+sm:rounded-lg
+`
 
+const Table = tw.table`
+w-full
+text-sm
+text-left
+text-gray-500
+dark:text-gray-400"
+`
 
+const Tr = tw.tr`
+bg-white
+border-b
+dark:bg-gray-900
+dark:border-gray-700
+`
+
+const Th = tw.th`
+px-6
+py-4
+font-medium
+text-gray-900
+whitespace-nowrap
+dark:text-white
+`
 const Block = ({ block }) => {
   const [currentTime,setCurrentTime] = useState("");
   useEffect(()=>{
@@ -17,86 +46,85 @@ const Block = ({ block }) => {
     <>
     {Object.keys(block).length > 0 && (
 
-<div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-<table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-
+<Div>
+<Table>
   <tbody>
-    <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-      <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+    <Tr>
+      <Th scope="row">
        Block Number: 
-      </th>
+      </Th>
       <td className="px-6 py-4">
         {block.number}
       </td>
-    </tr>
-    <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-      <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+    </Tr>
+    <Tr>
+      <Th scope="row" >
        Hash: 
-      </th>
+      </Th>
       <td className="px-6 py-4">
         {block.hash}
       </td>
-    </tr>
-    <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-      <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+    </Tr>
+    <Tr>
+      <Th scope="row">
        Parent Hash: 
-      </th>
+      </Th>
       <td className="px-6 py-4">
         {block.parentHash}
       </td>
-    </tr>
-    <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-      <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+    </Tr>
+    <Tr>
+      <Th scope="row">
        Time Stamp: 
-      </th>
+      </Th>
       <td className="px-6 py-4">
     
         {currentTime}
       </td>
-    </tr>
-    <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-      <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+    </Tr>
+    <Tr>
+      <Th scope="row">
        Gas Used: 
-      </th>
+      </Th>
       <td className="px-6 py-4">
         {Utils.formatUnits(block.gasUsed,"wei")} wei / {Utils.formatEther(block.gasUsed)} eth
       </td>
-    </tr>
-    <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-      <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+    </Tr>
+    <Tr>
+      <Th scope="row">
        Gas Limit: 
-      </th>
+      </Th>
       <td className="px-6 py-4">
       {Utils.formatUnits(block.gasLimit,"wei")} wei / {Utils.formatEther(block.gasLimit)} eth
       </td>
-    </tr>
-    <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-      <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+    </Tr>
+    <Tr>
+      <Th scope="row">
         Miner / Validator: 
-      </th>
+      </Th>
       <td className="px-6 py-4">
         {block.miner}
       </td>
-    </tr>
-    <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-      <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+    </Tr>
+    <Tr>
+      <Th scope="row">
        Fee: 
-      </th>
+      </Th>
       <td className="px-6 py-4">
       {Utils.formatUnits(block.baseFeePerGas,"wei")} wei / {Utils.formatEther(block.baseFeePerGas)} eth
       </td>
-    </tr>
-    <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-      <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+    </Tr>
+    <Tr>
+      <Th scope="row">
        Transactions: 
-      </th>
+      </Th>
       <td className="px-6 py-4">
         {Object(block.transactions).length} transactions
       </td>
-    </tr>
+    </Tr>
     </tbody>
-</table>
-</div>
+</Table>
+</Div>
 )}
     </>
     
